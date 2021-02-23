825642ad2e1b1a99a7a18231217582adfd6f3dec
anoyer <zhihuangliu@foxmail.com>
2021-02-23 23:43:15 +0800

    add utils

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 Anoyer All Rights Reserved.
+# Author: zhihuangliu@foxmail.com
+#
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
