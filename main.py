3bc4a5f099df3e45edae0da82b0dff7ea456c7fb
anoyer <zhihuangliu@foxmail.com>
2021-02-27 23:04:18 +0800

    add mysqltools

--- main.py
+++ main.py
@@ -5,29 +5,25 @@
 # Author: zhihuangliu@foxmail.com
 #
 
-from utils.common import read_json_from_file
 import pymysql
 import json
+
+from SQLtools import mysql
+from utils.common import read_json_from_file
+from utils import mysql_global_setting
 
 
 if __name__ == "__main__":
 
-    config_dict = read_json_from_file("CentralVision.conf")
-
-    db = pymysql.connect(host=config_dict['host'], user=config_dict['user'], password=config_dict['password'],
-                         database=config_dict['database'], charset=config_dict['charset'])
-    cursor = db.cursor()
+    mysql_global_setting.load_data()
+    mysql.start()
 
     sql = """
         SELECT * from test
         """
-    res = cursor.execute(sql)
-    result = cursor.fetchall()
-    print(list(zip(*cursor.description))[0])
+    result = mysql.select(sql)
     print(result)
 
-    cursor.close()
-
-    db.close()
+    mysql.terminate()
 
 
